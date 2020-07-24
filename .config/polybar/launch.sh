1c6e527c8056435ec02767a8f05860971c18f523
John Agapeyev <jagapeyev@fortinet.com>
2020-07-24 11:51:50 -0700

    Updated polybar config to play nicer with multimonitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
-
 # Terminate already running bar instances
 killall -q polybar
-
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar myi3bar &
+#setup environment
+export ETH=$(ip link | awk '{print $2}' | grep -Eo '^en[a-z0-9]*')
+export WIRE=$(ip link | awk '{print $2}' | grep -Eo '^wl[a-z0-9]*')
 
-echo "Bars launched..."
+# Launch polybar
+for i in $(polybar -m | awk -F: '{print $1}');
+do
+    export MONITOR=$i
+    polybar main -c ~/.config/polybar/config &
+done
