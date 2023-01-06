ffe65f22113b58d6e39616b3c5029eb6026cb1be
John Agapeyev <jagapeyev@gmail.com>
2023-01-05 17:01:45 -0800

    Update mpv settings, fix polybar networking on new desktop, and added venv exclusion to ctag generation in vim

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 #setup environment
-export ETH=$(ip link | awk '{print $2}' | grep -Eo '^en[a-z0-9]*')
-export WIRE=$(ip link | awk '{print $2}' | grep -Eo '^wl[a-z0-9]*')
+export ETH=$(ip link | awk '{print $2}' | grep -Eo '^en[a-z0-9]*' | sort -h | head -n 1)
+export WIRE=$(ip link | awk '{print $2}' | grep -Eo '^wl[a-z0-9]*' | sort -h | head -n 1)
 
 # Launch polybar
 for i in $(polybar -m | awk -F: '{print $1}');
